1986815deae83cb3b6aa15528377fe851728d7a7
dan-durkin <daniel.h.durkin@gmail.com>
2016-01-23 12:06:22 -0500

    Small formatting fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,21 +4,21 @@
 			.html5Mode({
 				enabled: true,
 				requireBase: false
-		});
+			});
 
-        $stateProvider
-            .state('active', {
-                url: '/',
-                controller: 'ActiveTaskCtrl as active',
-                templateUrl: '/templates/active.html'
-            })
+		$stateProvider
+			.state('active', {
+				url: '/',
+				controller: 'ActiveTaskCtrl as active',
+				templateUrl: '/templates/active.html'
+			})
 			.state('archive',{
 				url: '/archive',
 				controller: 'ArchiveCtrl as archive',
 				templateUrl: 'templates/archive.html'
 			});
 	}
-	
+
 	angular
 		.module('blocitoff', ['ui.router', 'firebase'])
 		.config(config);
